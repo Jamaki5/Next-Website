118cb778e56c1b37e14cb9ed0f52c9f358e4cec3
Jann <jamaki5@web.de>
2021-06-17 15:51:23 +0200

    fix(tools): added placeholder for JSON and Hash

--- components/Tools/Hash.js
+++ components/Tools/Hash.js
@@ -9,6 +9,7 @@ function Hash() {
   return (
     <div className="flex flex-col self-center w-full md:max-w-md h-full px-4 bg-white bg-opacity-10 rounded place-self-center">
       <h2 className="text-2xl mt-2">Hash</h2>
+      <div className="h-full w-full grid place-items-center text-3xl">Currently under construction</div>
     </div>
   );
 }
--- components/Tools/Json_decoder.js
+++ components/Tools/Json_decoder.js
@@ -8,6 +8,7 @@ function Json_decoder() {
   return (
     <div className="flex flex-col self-center w-full md:max-w-md h-full px-4 bg-white bg-opacity-10 rounded place-self-center">
       <h2 className="text-2xl mt-2">JSON</h2>
+      <div className="h-full w-full grid place-items-center text-3xl">Currently under construction</div>
     </div>
   );
 }
